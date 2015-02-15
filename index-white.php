7ea26ac2342a6b73dcaf3d60d29f886fdfefc4ea
sansha <sansha98@gmail.com>
2015-02-15 20:58:28 +0100

    Update index-white.php

--- index-white.php
+++ index-white.php
@@ -9,6 +9,9 @@ Licht an und ausschalten:
 <input type="submit" value="Licht ein" name="Lichtein">
 <input type="submit" value="Licht aus" name="Lichtaus">
 </form>
+<form action="slot_act.php" method="post">
+Time: <input name="dtime" size="7" maxlenght="4"> <!-- Hier sollen nur Zahlen rein -->
+
 immer erst ausschalten, wenn 'Licht ist an' dasteht und umgekehrt! <br>
 nach druecken des Knopfes WARTEN bis die Aktion abgeschlossen ist und das Ergebnis 'Licht ist aus/an' angezeigt wird!
 <br> <br>
