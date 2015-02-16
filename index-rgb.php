972883744c8ddf3202b8b5a7f344dbbb286d9113
Daniel Bücheler <dbahn25@t-online.de>
2015-02-16 01:33:54 +0100

    new index.php for rgb (index-rgb.php)

--- index-rgb.php
+++ index-rgb.php
@@ -1,73 +1,37 @@
-
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
+<head>
 <meta charset="UTF-8">
-
+<title>LED-Steuerung</title>
+</head>
 <body>
-
-<h1>Beleuchtungssteuerung</h1>
-
-<?php
-	echo "Hello World";
-?>
-<form action="index.php" method="GET">
-<table>
+<form action="" method="GET">
+	<table>
 	<tr>
-		<td><input type="radio" value="an" name="r">Rot an
-		<td><input type="radio" value="an" name="g"/>Grün an
-		<td><input type="radio" value="an" name="b"/>Blau an
+		<td><input type="radio" value="100" name="r">Rot an
+		<td><input type="radio" value="100" name="g"/>Grün an
+		<td><input type="radio" value="100" name="b"/>Blau an
 	</tr>
 	<tr>
-		<td><input type="radio" value="aus" name="r">Rot aus
-		<td><input type="radio" value="aus" name="g">Grün aus
-		<td><input type="radio" value="aus" name="b">Blau aus
+		<td><input type="radio" value="0" name="r" checked>Rot aus
+		<td><input type="radio" value="0" name="g" checked>Grün aus
+		<td><input type="radio" value="0" name="b" checked>Blau aus
 	</tr>
 </table>
-<br>
-<input type=submit value="Lichter schalten"/>
+	<br>
+	<input type="checkbox" value="1" name="fade" checked>Fade
+	<input type="submit" value="LEDs schalten">
 </form>
+
 <?php
-$pin_red = 17;
-$pin_green = 18;
-$pin_blue = 22;
-function switch_leds($pin, $value) {
-        $cmd = "echo \"$pin=$value\" > /dev/pi-blaster";
-	echo shell_exec($cmd);
-}
-$red = $_GET["r"];
-if($red != ""){
-	echo "Rot: $red<br>";
-	if ($red == "an") {
-        	switch_leds($pin_red, 1);
-	} elseif ($red == "aus") {
-        	switch_leds($pin_red, 0);
-	}
-} else {
-	echo "Rot: Zustand nicht bekannt<br>";
-}
-$green = $_GET["g"];
-if($green != "") {
-	echo "Grün: $green<br>";
-	if ($green == "an") {
-		switch_leds($pin_green, 1);
-	} elseif ($green == "aus") {
-		switch_leds($pin_green, 0);
-	}
-} else {
-	echo "Grün: Zustand nicht bekannt<br>";
-}
-$blue = $_GET["b"];
-if($blue != "") {
-	echo "Blau: $blue<br>";
-	if ($blue == "an") {
-		switch_leds($pin_blue, 1);
-	} elseif ($blue == "aus") {
-		switch_leds($pin_blue, 0);
-	}
-} else {
-	echo "Blau: Zustand nicht bekannt<br>";
-}	
+	$r = $_GET["r"];
+	$g = $_GET["g"];
+	$b = $_GET["b"];
+	$fade = $_GET["fade"];
+
+	$cmd = "./universal.py $fade $r $g $b";
+	shell_exec($cmd);
 ?>
-<br>
+
 </body>
-</html>
+</html
