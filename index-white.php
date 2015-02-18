0cdcf9fe025b5d3db9e61674bac21e0f9a382dd5
sansha <sansha98@gmail.com>
2015-02-18 18:54:33 +0100

    Update index-white.php

--- index-white.php
+++ index-white.php
@@ -6,8 +6,9 @@
 <body>
 Licht an und ausschalten:
 <form action="" method="get">
-  <input type="radio" value="1" name="w">Licht ein  <br>
-  <input type="radio" value="0" name="w">Licht aus <br> <br> 
+  <input type="radio" value="0" name="w">0% Helligkeit  <br>
+  <input type="radio" value="50" name="w">50% Helligkeit <br>
+  <input type="radio" value="100" name="w">100% Helligkeit <br> 
   targetLuminance eingeben: 0 bis 100: 
   <input type="number" default="100" name="luminance" min="0" max="100">
   <br> 
@@ -27,36 +28,29 @@ nach druecken des Knopfes WARTEN bis die Aktion abgeschlossen ist und das Ergebn
 <br> <br>
 
 <?php
-//echo "hello world1";
+
 $fade=$_GET['fade'];
 $white=$_GET['w'];
-//$luminance=$_GET['luminance'];
-
-//echo "Hello World   fade:";
-//echo $fade;
+$luminance=$white;
 
 if($fade == "") {
-  $fade=1;  }   // default is on
+  $fade=1;  }   // default is on || immer an, da 0 in python noch nicht realisiert ist
 
-//if ($white==1) {   //licht an
-//   $luminance=100;
-//}//
-//$luminance=0;
-//echo $luminance;
+
 
 //$alternativeLuminance=$_GET['luminance'];
 //if($alternativeLuminance!="")  {
-	$luminance=$_GET['luminance'];
+$luminance=$_GET['luminance'];
 //}
 //echo $luminance;
-if ($white==1) {
+/*if ($white==100) {
 	$luminance=100;
 } 
 //echo $luminance;
 
 if ($white==0) {   //licht aus
   $luminance=0;
-} 
+} */
 echo $luminance;
  if($fade==1) {
     $cmd = "sudo ./universal.py 1 $luminance 0 0 0"; 
