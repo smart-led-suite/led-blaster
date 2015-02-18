55e676e44ef1dcdf698e71e1cfb16dade60234f6
root <root@raspberrypi.(none)>
2015-02-18 10:45:53 +0000

    variable funktioniert vollständig, luminance eingabefeld noch nicht.

--- index-white.php
+++ index-white.php
@@ -8,9 +8,9 @@ Licht an und ausschalten:
 <form action="" method="get">
   <input type="radio" value="1" name="w">Licht ein  <br>
   <input type="radio" value="0" name="w">Licht aus <br> <br> 
-<!--  targetLuminance eingeben: 0 bis 100: 
-  <input type="number" name="luminance" min="0" max="100">
-  <br> -->
+  targetLuminance eingeben: 0 bis 100: 
+  <input type="number" default="100" name="luminance" min="0" max="100">
+  <br> 
   <input type="radio" name="fade" value="0" checked>kein Fade
   <br>
   <input type="radio" name="fade" value="1" checked>Fade
@@ -38,35 +38,36 @@ $white=$_GET['w'];
 if($fade == "") {
   $fade=1;  }   // default is on
 
-if ($white==1) {   //licht an
-   $luminance=100;
-}
- /* if($fade==1) {
-    $cmd = "sudo  ./universal.py 1 $luminance "; 
-    echo $cmd;
-    $val =  shell_exec($cmd); 
-    echo "Licht ist an";
-    echo $luminance;
+//if ($white==1) {   //licht an
+//   $luminance=100;
+//}//
+//$luminance=0;
+//echo $luminance;
+
+//$alternativeLuminance=$_GET['luminance'];
+//if($alternativeLuminance!="")  {
+	$luminance=$_GET['luminance'];
+//}
+//echo $luminance;
+if ($white==1) {
+	$luminance=100;
 } 
-  else { 
-    $cmd = "sudo ./universal.py 1 100 0 0"; 
-    echo $cmd;
-    $val =  shell_exec($cmd); 
-    echo "Licht ist an";  } 
-}*/
+//echo $luminance;
+
 if ($white==0) {   //licht aus
   $luminance=0;
 } 
+echo $luminance;
  if($fade==1) {
-    $cmd = "sudo ./universal.py 1 $luminance 0 0"; 
+    $cmd = "sudo ./universal.py 1 $luminance 0 0 0"; 
     echo $cmd;
     $val =  shell_exec($cmd); 
     echo $val;
-    echo "Licht ist aus";
+   echo "Licht ist aus";
     echo $luminance;
 }
   else { 
-    $cmd = "sudo ./universal.py 1 $luminance "; 
+    $cmd = "sudo ./universal.py 1 $luminance 0 0 0"; 
     echo $cmd;
     $val =  shell_exec($cmd); 
     echo "Licht ist aus";  } 
