04b6588ce307116468678b3cba401672083af535
root <root@raspberrypi.(none)>
2015-02-18 10:26:07 +0000

    luminance variable eingeführt, weniger schleifen

--- index-white.php
+++ index-white.php
@@ -6,7 +6,7 @@
 <body>
 Licht an und ausschalten:
 <form action="" method="get">
-  <input type="radio" value="1" name="w">Licht ein 
+  <input type="radio" value="1" name="w">Licht ein  <br>
   <input type="radio" value="0" name="w">Licht aus <br> <br> 
 <!--  targetLuminance eingeben: 0 bis 100: 
   <input type="number" name="luminance" min="0" max="100">
@@ -37,33 +37,40 @@ $white=$_GET['w'];
 
 if($fade == "") {
   $fade=1;  }   // default is on
+
 if ($white==1) {   //licht an
-  if($fade==1) {
-    $cmd = "sudo  ./universal.py 1 100 "; 
+   $luminance=100;
+}
+ /* if($fade==1) {
+    $cmd = "sudo  ./universal.py 1 $luminance "; 
     echo $cmd;
     $val =  shell_exec($cmd); 
     echo "Licht ist an";
-}
+    echo $luminance;
+} 
   else { 
     $cmd = "sudo ./universal.py 1 100 0 0"; 
     echo $cmd;
     $val =  shell_exec($cmd); 
     echo "Licht ist an";  } 
-}
+}*/
 if ($white==0) {   //licht aus
-  if($fade==1) {
-    $cmd = "sudo ./universal.py 1 0 0 0"; 
+  $luminance=0;
+} 
+ if($fade==1) {
+    $cmd = "sudo ./universal.py 1 $luminance 0 0"; 
     echo $cmd;
     $val =  shell_exec($cmd); 
     echo $val;
     echo "Licht ist aus";
+    echo $luminance;
 }
   else { 
-    $cmd = "sudo ./universal.py 1 0 "; 
+    $cmd = "sudo ./universal.py 1 $luminance "; 
     echo $cmd;
     $val =  shell_exec($cmd); 
     echo "Licht ist aus";  } 
-}
+
 //else if (isset($_GET['w_off'])) {
 //echo "Hallo 2";
 /*$cmd = "./universal.py 1 1 0 0 0 ";
