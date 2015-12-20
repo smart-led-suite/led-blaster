b44f856eed92cb4cc70ccdc0da66a73039fb0d87
sansha <sansha98@gmail.com>
2015-12-20 21:33:53 +0000

    update for better user experience with telemetry currentBrightness is now saved before fading so the webapp/app can load the correct values

--- led-blaster-pre.cpp
+++ led-blaster-pre.cpp
@@ -4,7 +4,7 @@
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
-// ToDo	       : 
+// ToDo	       :
 //	(done)		 - new universal initPin
 //	(done)		 - introduce header file so the functions can be placed below the main part
 //	(done)		 - introduce file to save current luminances
@@ -61,109 +61,115 @@ int PWMrange = 0;
 uint16_t mode = 0; // mode is now a global variable! set mode to 0 (default)
 
 //uint16_t targetBrightness[4]; //global because of readCurrentBrightness || maybe changed in the future since we won't need the cBFile in the future
-//uint16_t currentBrightness[4]; 
+//uint16_t currentBrightness[4];
 
 
-map < std::string, int> pin = 
+map < std::string, int> pin =
 {
-	{"w", 25}, 
+	{"w", 25},
+	{"w1", 26},
 	{"r", 17},
 	{"g", 18},
+	{"g2", 19},
 	{"b", 22}
 };
-map < string, int> ledsTarget = 
+map < string, int> ledsTarget =
 {
-	{"w", 0}, 
+	{"w", 0},
+	{"w1", 26},
 	{"r", 0},
 	{"g", 0},
+	{"g2", 19},
 	{"b", 0}
 };
-map < string, int> ledsCurrent = 
+map < string, int> ledsCurrent =
 {
-	{"w", 0}, 
+	{"w", 0},
+	{"w1", 26},
 	{"r", 0},
 	{"g", 0},
+	{"g2", 19},
 	{"b", 0}
 };
 
 
 //***********************************************************************************************
 //********************************************** MAIN *********************************************
-//*********************************************************************************************	
+//*********************************************************************************************
 
 int main(int argc, char* argv[]) {
 	bool mode1ThreadActive = false; //default is: no thread (obviusly)
 	int threadErrorNumber = 0; //variable for pthread_create, if its 0 there was no problem
 	//init variables to use with the interactive live input
-	
+
   	uint16_t brightness;
-  	uint16_t waitCounter = 0; //used only in live mode. 
+  	uint16_t waitCounter = 0; //used only in live mode.
 	int fadeTimeMs = 1000; //time variable in ms; default is 1000
-	
-	
+
+
 	// FIFO vars
 	FILE *fifo_file;
-	
+
 	char *lineptr = NULL, nl; // pointer to line currently being read
 	size_t linelen; // length of line read
 	int numberOfValues; // needed to check if anything was read, and how many parts were detected during sscanf
 	// Commands are composed as following: "COMMAND=VALUE", e.g. b=50 for blue, brightness 50
 	char cmd[] = "                   "; // command (before "=")
 	int value; // brightness (after "=")
-	
+
 	int sleep = 50000; // us to sleep
-	
+
 	char dummy[] = "hallo"; //for whatever reason the last defined char array will be smashed into rubbish after while(true). so therefore a dummy as last char
-	
+
   	//init variables needed for thread creation
-  	
-	
+
+
 	//OPEN CONFIG FILE IN OUR APPLICAITONS DIRECTORY OR CREATE IT IF IT DOESN'T EXIST
 	readCurrentBrightness();
-	
+
 	//init pwm
 	//initializes the pigpio libary. returns 0 if there was no problem
-	if(initGeneral()) {				
+	if(initGeneral()) {
 		//if there's a problem print it out and exit the program
-		cout << "initGeneral failed!" << endl;	
+		cout << "initGeneral failed!" << endl;
 		return 1;
 	}
-	
+
 	//initialize all pins
 	//every color from the pin map
 	for(auto const &colors : pin) {
 		//initializes each pin. returns 0 if everything went ok
-		if(initPin(colors.second, mode) != 0) {  				
+		if(initPin(colors.second, mode) != 0) {
 			//print that there has been an error if this happens (very unlikely)
-			cout << "error in initPin " << colors.second; 
+			cout << "error in initPin " << colors.second;
 			cout << "which is used by color " << colors.first << endl;
-			//and exit the program	
-			return 2;						
-		}	
+			//and exit the program
+			return 2;
+		}
 	}
-	
-	
-	
-	//if ctrl+c is pressed we want to terminate the gpios and close all open threads. 
+
+
+
+	//if ctrl+c is pressed we want to terminate the gpios and close all open threads.
 	//therefore we'll want to catch the ctrl+c by the user
 	//signal(SIGINT, terminateLedBlaster);
 	signal(SIGINT, ledBlasterTerminate);
-	//if the kill command is send (SIGTERM) we want to terminate the gpios and close all 
+	//if the kill command is send (SIGTERM) we want to terminate the gpios and close all
 	//open threads a bit faster than if ctrl+c is detected
 	signal(SIGTERM, ledBlasterTerminateFast);
-	
-	
-	
-	
+
+
+
+
 	// FIFO preparations
-        //Create the FIFO if it does not exist 
+        //Create the FIFO if it does not exist
         umask(0);
         mknod(FIFO_FILE, S_IFIFO|0666, 0);
 
         fifo_file = fopen(FIFO_FILE, "r");
-	
+
 	cout << "led-blaster has successfully started." << endl;
-	
+
 	while(true) {
 		// READ COMMAND + VALUE
 		if ((numberOfValues = getline(&lineptr, &linelen, fifo_file)) < 0) { // if no lines read -> wait and repeat
@@ -172,11 +178,11 @@ int main(int argc, char* argv[]) {
 		}
 		printf("[%d]%s\n", numberOfValues, lineptr);
 		numberOfValues = sscanf(lineptr, "%[^=]=%d%c", &cmd, &value, &nl);
-		
+
 		printf("numberOfValues: %d, cmd: %s, value: %d\n", numberOfValues, cmd, value);
 		//if we have the variable=value syntax we have 3 returned by sscanf
 		//only in this case we want to do anything.
-		if (numberOfValues == 3) 
+		if (numberOfValues == 3)
 		{
 			// PROCESS COMMAND
 		  	if (strcmp("exit", cmd)==0) //strcmp compares c strings and returns 0 if they are the same
@@ -184,7 +190,7 @@ int main(int argc, char* argv[]) {
 		  		ledBlasterTerminate(0); //terminates LEDBlaster. function needs an int as argument which is useless (but necessary for signal() syntax
 				//return 0;
 		  	}
-		  	else  if (strcmp("mode", cmd)==0) 
+		  	else  if (strcmp("mode", cmd)==0)
 		  	{
 		  		mode = value;
 		  	}
@@ -193,35 +199,37 @@ int main(int argc, char* argv[]) {
 				fadeTimeMs = value;
 			}
 			if (mode == 0) { //mode statement is here because we only need these cmds in mode0
-			  	if (strcmp("wait", cmd)==0) 
+			  	if (strcmp("wait", cmd)==0)
 			  	{
 			  		waitCounter = value;
 			  	}
-			  	else 
+			  	else
 			  	{
 			  		auto cmdInMap = ledsTarget.find(cmd);
 			  		if (cmdInMap != ledsTarget.end())
 			  		{
 			  			ledsTarget[cmd] = value;
-			  		} 
+			  		}
 			  		else
 			  		{
 			  			cout << "input couldnt be detected" << endl;
 			  		}
-			  		if (waitCounter) 
+			  		if (waitCounter)
 			  		{
 				  		waitCounter--;
 				  	}
-				  		
-			  	} 
-			  	
-		  	
+
+			  	}
+
+
 		  		if (waitCounter == 0) {
 		  			cout << "fading leds simultaneous..." << endl;
-			  		fadeSimultaneous(fadeTimeMs);
+			  		//write to file before fading
+					writeCurrentBrightness();
+					fadeSimultaneous(fadeTimeMs);
 			  		//fadeDirectly(); //for testing purposes
 			  		//write brightness so php part can read it :-)
-			  		writeCurrentBrightness(); 
+			  		//writeCurrentBrightness();
 			  		cout << "fading leds simultaneous finished" << endl;
 			  	}
 			  	//print some debug info of the variables
@@ -235,56 +243,56 @@ int main(int argc, char* argv[]) {
   				}
 				mode1ThreadActive = false; 	//make it possible to start mode 1 again
 			}
-		  
-			else if (mode == 1) 
+
+			else if (mode == 1)
 		  	{
-		  		if (mode1ThreadActive == false) 
+		  		if (mode1ThreadActive == false)
 		  		{
-			  		//for documentation about threads see: 
+			  		//for documentation about threads see:
 			  		//https://computing.llnl.gov/tutorials/pthreads/
-			  		//and about the pthread_create function see:  
+			  		//and about the pthread_create function see:
 			  		//https://computing.llnl.gov/tutorials/pthreads/man/pthread_create.txt
 			  		pthread_t mode1Thread; //create reference variable for the thread
 			  		//it is recommended to not use a address as attribute because the value
 			  		//may change until the thread is started (it's listed as bad example in the documentation.
-			  		//but as we WANT to change it after the thread has started we're using it anyway. 
+			  		//but as we WANT to change it after the thread has started we're using it anyway.
 			  		//if there are any problems we comment MODE_LIVE_MANIPULATING and everything is as its recommended.
 			  		#ifndef MODE_LIVE_MANIPULATING
-			  			//init the new thread, it can be adressed by mode1Thread, the fadeTimeUs variable will 
-			  			//be sent as attribute, the main function of the new thread is mode1 
-			  			threadErrorNumber = pthread_create(&mode1Thread, NULL, mode1 , (void *) fadeTimeMs); 
+			  			//init the new thread, it can be adressed by mode1Thread, the fadeTimeUs variable will
+			  			//be sent as attribute, the main function of the new thread is mode1
+			  			threadErrorNumber = pthread_create(&mode1Thread, NULL, mode1 , (void *) fadeTimeMs);
 			  		#endif
 			  		#ifdef MODE_LIVE_MANIPULATING
 			  			//init the new thread, it can be adressed by mode1Thread
 			  			//the fadeTimeUs variable will be sent as attribute
-			  			//the main function of the new thread is mode1 
-			  			threadErrorNumber = pthread_create(&mode1Thread, NULL, mode1 , (void *) &fadeTimeMs); 
+			  			//the main function of the new thread is mode1
+			  			threadErrorNumber = pthread_create(&mode1Thread, NULL, mode1 , (void *) &fadeTimeMs);
 			  		#endif
 			  		if (threadErrorNumber != 0)  //if it is successfull it returns 0, otherwise an error code
 			  		{
 			  			cout << "Something happened while starting the mode1 Thread" << endl;
 			  			cout << "error no: " << threadErrorNumber << endl;		//print the errorcode if one occurs
 			  			threadErrorNumber = 0; //reset error number for future restarts
-			  		} 
+			  		}
 			  		else 							//else, the thread was successfully created
 			  		{
 			  		mode1ThreadActive = true; //set variable to true because thread has been created successfully
 			  		cout << "start continuos, random fade on all pins (wrgb successively) exit with mode = 0 or Ctrl+C, DO NOT CHANGE ANY OTHER VALUE OR YOU HAVE TO REBOOT YOUR PI" << endl;
 			  		}
 		  		}
-		  					
+
 			}
 			gpioSleep(PI_TIME_RELATIVE, 0, 100000); //sleeps for 0.1s
-			//gpioDelay(10000); //10ms some delay so it won't use that much cpu power 
+			//gpioDelay(10000); //10ms some delay so it won't use that much cpu power
 		}
-			
-	} // END NOW-INVALID CODE 
+
+	} // END NOW-INVALID CODE
 }
 
 
 //***************************************FUNCTIONS***************************************
 
-//function which is called if ctrl+c is pressed in order to close led-blaster normally	
+//function which is called if ctrl+c is pressed in order to close led-blaster normally
 void ledBlasterTerminate(int dummy)
 {
 	mode = 0; //so we won't have any problems with threads and so on
@@ -314,4 +322,3 @@ void ledBlasterTerminateFast(int dummy)
 	printf("exit program. thank you. \n");
   	exit(1);
 }
-
